310592228aacfeb3589bde972d2ee30b7878435f
Quang Anh <quanganhk62@gmail.com>
2021-11-21 13:36:16 +0700

    Recommend backend

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,14 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
-    
+
     protected $fillable = [
         'name',
         'image_link',
         'price',
         'description',
-        'category_id'
+        'category_id',
     ];
+
+    protected $appends = ['recommend_mark'];
 
     public $timestamps = true;
 
@@ -27,5 +29,22 @@ class Product extends Model
     public function reviews()
     {
         return $this->hasMany(Review::class);
+    }
+
+    public function getRecommendMarkAttribute()
+    {
+        $review_count = $this->reviews->count();
+        $rating_count = $this->reviews->sum('rating');
+        /* CACH TINH DIEM RECOMMEND
+        Moi review tang 1 diem recommend
+        rating 1 sao -2
+        rating 2 sao -1
+        rating 3 sao 0
+        rating 4 sao +1
+        rating 5 sao +2
+        => Tong diem
+         */
+        $recommend_mark = $rating_count - 2 * $review_count;
+        return $recommend_mark;
     }
 }
