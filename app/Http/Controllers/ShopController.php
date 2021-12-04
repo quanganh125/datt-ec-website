63291bd4b7a8d2c50b3285c9b03f6138e74daab3
tvhung <truonghungvatvo@gmail.com>
2021-12-04 16:37:26 +0700

    edit backend shop

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -8,6 +8,7 @@ use App\Http\Resources\ShopResource;
 use App\Http\Resources\ShopCollection;
 use App\Services\ShopService;
 use App\Services\UserService;
+use App\Services\ProductService;
 use Validator;
 use Illuminate\Support\Facades\Auth;
 
@@ -15,11 +16,13 @@ class ShopController extends Controller
 {
     protected $shopService;
     protected $userService;
+    protected $productService;
 
-    public function __construct(ShopService $shopService, UserService $userService)
+    public function __construct(ShopService $shopService, UserService $userService, ProductService $productService)
     {
         $this->shopService = $shopService;
         $this->userService = $userService;
+        $this->productService = $productService;
     }
     /**
      * Display a listing of the resource.
@@ -54,8 +57,8 @@ class ShopController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => 'bail|required|string|max:255',
             'address' => 'bail|required|string|max:255',
-            'logo' => 'bail|string',
-            'url' => 'bail|string',
+            'logo' => 'bail|required|string',
+            'url' => 'bail|required|string',
         ]);
 
         if($validator->fails()){
@@ -63,12 +66,13 @@ class ShopController extends Controller
         }
         // $user_id = auth()->user()->id;
         $user_id = Auth::user()->id;
+        $logo_storage = $this->productService->saveImgBase64($request->input('url'), 'product_img');
 
         $shop = new Shop();
         $shop->name = $request->input('name');
         $shop->address = $request->input('address');
         $shop->logo = $request->input('logo');
-        $shop->url = $request->input('url');
+        $shop->url = $logo_storage;
         $shop->user_id = $user_id;
         $shop->save();
         return (new ShopResource($shop))->response();
@@ -109,8 +113,8 @@ class ShopController extends Controller
         $validator = Validator::make($request->all(), [
             'name' => 'bail|required|string|max:255',
             'address' => 'bail|required|string|max:255',
-            'logo' => 'bail|string',
-            'url' => 'bail|string',
+            'logo' => 'bail|required|string',
+            'url' => 'bail|required|string',
         ]);
 
         if($validator->fails()){
@@ -118,6 +122,8 @@ class ShopController extends Controller
         }
 
         $input = $request->all();
+        $logo_storage = $this->productService->saveImgBase64($input('url'), 'product_img');
+        $input["url"] = $logo_storage;
         $shop = $this->shopService->update($id, $input);
         return (new ShopResource($shop))->response();
     }
