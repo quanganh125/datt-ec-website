d5c7eb466be9e82a60a0592cb3c8906e3df6f79d
quanganh <quanganhk62@gmail.com>
2022-07-13 18:46:20 +0700

    Set init state, remove unnecessary element

--- resources/js/components/layouts/CheckMark/index.js
+++ resources/js/components/layouts/CheckMark/index.js
@@ -16,21 +16,18 @@ export default function CheckMark({ stateCopy, setStateCopy }) {
         return () => {};
     }, [stateCopy]);
 
-    return ( <
-        > { " " } {
-            isShow && ( <
-                div className = "success-checkmark" >
-                <
-                div className = "check-icon" >
-                <
-                span className = "icon-line line-tip" > < /span>{" "} <
-                span className = "icon-line line-long" > < /span>{" "} <
-                div className = "icon-circle" > < /div>{" "} <
-                div className = "icon-fix" > < /div>{" "} <
-                /div>{" "} <
-                /div>
-            )
-        } { " " } <
-        />
+    return (
+        <>
+            {isShow && (
+                <div className="success-checkmark">
+                    <div className="check-icon">
+                        <span className="icon-line line-tip"> </span>
+                        <span className="icon-line line-long"> </span>
+                        <div className="icon-circle"> </div>
+                        <div className="icon-fix"> </div>
+                    </div>
+                </div>
+            )}
+        </>
     );
 }
